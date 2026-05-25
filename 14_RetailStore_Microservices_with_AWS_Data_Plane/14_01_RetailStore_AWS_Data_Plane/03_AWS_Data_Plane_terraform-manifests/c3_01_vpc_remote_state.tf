0e3f35ebfb1452bf210eb43a29d3d59e8f6f493b
tcollins520 <tcollins520@gmail.com>
2026-05-25 12:38:54 -0400

    Implemented Terraform-based AWS data plane with RDS, DynamoDB, ElastiCache, SQS, and pod identity

---
+++ 14_RetailStore_Microservices_with_AWS_Data_Plane/14_01_RetailStore_AWS_Data_Plane/03_AWS_Data_Plane_terraform-manifests/c3_01_vpc_remote_state.tf
@@ -0,0 +1,38 @@
+# --------------------------------------------------------------------
+# Reference the Remote State from VPC Project
+# Will be used to create resources in the same VPC and subnets as the EKS cluster
+# --------------------------------------------------------------------
+# ----------------------------------------------------------------
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+
+  config = {
+    bucket = "tfstate-dev-us-east-1-bl6qlh"     # Name of the remote S3 bucket where the VPC state is stored
+    key    = "vpc/dev/terraform.tfstate"        # Path to the VPC tfstate file within the bucket
+    region = var.aws_region                    # Region where the S3 bucket exist
+  }
+}
+
+# --------------------------------------------------------------------
+# Output the VPC ID from the remote VPC state
+# --------------------------------------------------------------------
+output "vpc_id" {
+  value = data.terraform_remote_state.vpc.outputs.vpc_id
+}
+
+# --------------------------------------------------------------------
+# Output the list of private subnets from the VPC
+# --------------------------------------------------------------------
+output "private_subnet_ids" {
+  value = data.terraform_remote_state.vpc.outputs.private_subnet_ids
+}
+
+
+# --------------------------------------------------------------------
+# Output the list of public subnets from the VPC
+# --------------------------------------------------------------------
+output "public_subnet_ids" {
+  value = data.terraform_remote_state.vpc.outputs.public_subnet_ids
+}
+
+
